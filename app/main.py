8957629e49f39a86f0f5c42fb7759501c49f931d
Noussai24 <noussaiba.ayadi@gmail.com>
2024-05-07 16:17:16 +0200

    📦 NEW: 1er api openIA

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
-
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status":"ok"}
+
+
+
+
+client = OpenAI(
+    api_key=os.environ.get("OPEN_API_KEY"),
+)
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
